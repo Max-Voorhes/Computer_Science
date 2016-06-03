7fd2fec0bad6621d98208a9cfa13450418b0962c
max_voorhes <mvoorhes.sh@mysummitps.org>
2016-06-03 20:21:11 +0000

    update

--- info.js
+++ info.js
@@ -33,13 +33,13 @@ item1.addEventListener('click', function(){
         document.getElementById('moreInfo9').innerHTML = "Self Defense Training Camp is a good time for all, just look at the people in the bottom right, they sure look like they're having fun.";
     } );
     info3.addEventListener("click", function(){
-        document.getElementById('moreInfo8').innerHTML = "Fighter Within is one of the best fighting games, by far better than street fighter and so.. so much better than mortal kombat (stay away from that game).";
+        document.getElementById('moreInfo8').innerHTML = "Fighter Within is one of the best fighting games, by far better than street fighter and is so much better than mortal kombat (stay away from that game).";
     } );
     info4.addEventListener("click", function(){
         document.getElementById('moreInfo7').innerHTML = "Battlefield 4 was truly something to admire when it was first released. With the smooth performance and great graphics at launch, it's a shame the developers threw all that out the window with the constant patches, ruining the game for good.";
     } );
     info5.addEventListener("click", function(){
-        document.getElementById('moreInfo6').innerHTML = "Tag is a game anyone can play, whether at work, at home, at school, or in a bedroom";
+        document.getElementById('moreInfo6').innerHTML = "Tag is a game anyone can play, whether at work, at home, at school, or in a bedroom.";
     } );
     info6.addEventListener("click", function(){
         document.getElementById('moreInfo5').innerHTML = "The games on this console are so good, I just put the entire console on the list.";
@@ -64,16 +64,16 @@ item1.addEventListener('click', function(){
 });
                        
 item2.addEventListener('click', function(){
-    info1.innerHTML = '<p>10. Half-Life 3</p> <img src="https://i.ytimg.com/vi/VtI5HM7GVGY/maxresdefault.jpg" width="500" title="Bad graphics means bad game.">';
-    info2.innerHTML = '<p>9. Any nintendo game</p> <img src="http://0.media.dorkly.cvcdn.com/46/33/df85161096d4da2defc3fec59edf8c5a-20-more-of-the-worst-videogame-covers-of-all-time.jpg" width="500px" title="They only make educational games.">';
-    info3.innerHTML = '<p>8. Skyrum</p> <img src="http://i.ytimg.com/vi/niZKDa9LhRM/maxresdefault.jpg" width="500" title="Again, bad graphics means bad game.">';
-    info4.innerHTML = '<p>7. Left 4 ded</p> <img src="http://gadgetsin.com/uploads/2012/08/left_4_dead_boomer_action_figure_2.jpg" title="Left 4 dead fanbase">';
-    info5.innerHTML = '<p>6. Mortal Combat</p> <img src="http://media.giphy.com/media/N7DzaNFYK7888/giphy.gif" title="Yet again, bad graphics means bad game.">';
-    info6.innerHTML = '<p>5. Soccer</p> <img src="https://lh4.ggpht.com/fOB4Z0XH2yX7FhOjX1M8gX-m9VUCNbMyTuIUYcccwEVgQ-kAzBgF9RINa6Aeq7FpByuf=w300" title="Isnt even a real sport">';
-    info7.innerHTML = '<p>4. Tetris</p> <img src="http://www.gifbin.com/bin/102009/1255604935_tetris_fail.gif" width="400" title="Actual Tetris Gameplay">';
-    info8.innerHTML = '<p>3. Freeze Tag</p> <img src="http://vignette4.wikia.nocookie.net/batman/images/7/7c/Mr-freeze-evil-plan.jpg/revision/latest?cb=20120430121640" title="Couldnt find any pictures of Freeze Tag so I guess Ill have to use this picture for now.">';
-    info9.innerHTML = '<p>2. Battlefield 3</p> <img src="https://i.ytimg.com/vi/dzOWigk9Byw/maxresdefault.jpg" width="400" title="Those glitches though">';
-    info10.innerHTML = '<p>1. Youtube Comment Section</p> <img src="https://uproxx.files.wordpress.com/2013/09/youtube.jpg?w=650" title="TrollTube">';
+    info1.innerHTML = '<p>10. Half-Life 3</p> <img src="https://i.ytimg.com/vi/VtI5HM7GVGY/maxresdefault.jpg" width="500" title="Bad graphics means bad game."> <p id="moreInfo10"> </p>';
+    info2.innerHTML = '<p>9. Any nintendo game</p> <img src="http://0.media.dorkly.cvcdn.com/46/33/df85161096d4da2defc3fec59edf8c5a-20-more-of-the-worst-videogame-covers-of-all-time.jpg" width="500px" title="They only make educational games."><p id="moreInfo9"> </p>';
+    info3.innerHTML = '<p>8. Skyrum</p> <img src="http://i.ytimg.com/vi/niZKDa9LhRM/maxresdefault.jpg" width="500" title="Again, bad graphics means bad game."><p id="moreInfo8"> </p>';
+    info4.innerHTML = '<p>7. Left 4 ded</p> <img src="http://gadgetsin.com/uploads/2012/08/left_4_dead_boomer_action_figure_2.jpg" title="Left 4 dead fanbase"><p id="moreInfo7"> </p>';
+    info5.innerHTML = '<p>6. Mortal Combat</p> <img src="http://media.giphy.com/media/N7DzaNFYK7888/giphy.gif" title="Yet again, bad graphics means bad game."><p id="moreInfo6"> </p>';
+    info6.innerHTML = '<p>5. Soccer</p> <img src="https://lh4.ggpht.com/fOB4Z0XH2yX7FhOjX1M8gX-m9VUCNbMyTuIUYcccwEVgQ-kAzBgF9RINa6Aeq7FpByuf=w300" title="Isnt even a real sport"><p id="moreInfo5"> </p>';
+    info7.innerHTML = '<p>4. Tetris</p> <img src="http://www.gifbin.com/bin/102009/1255604935_tetris_fail.gif" width="400" title="Actual Tetris Gameplay"><p id="moreInfo4"> </p>';
+    info8.innerHTML = '<p>3. Freeze Tag</p> <img src="http://vignette4.wikia.nocookie.net/batman/images/7/7c/Mr-freeze-evil-plan.jpg/revision/latest?cb=20120430121640" title="Couldnt find any pictures of Freeze Tag so I guess Ill have to use this picture for now."><p id="moreInfo3"> </p>';
+    info9.innerHTML = '<p>2. Battlefield 3</p> <img src="https://i.ytimg.com/vi/dzOWigk9Byw/maxresdefault.jpg" width="400" title="Those glitches though"><p id="moreInfo2"> </p>';
+    info10.innerHTML = '<p>1. Youtube Comment Section</p> <img src="https://uproxx.files.wordpress.com/2013/09/youtube.jpg?w=650" title="TrollTube"><p id="moreInfo1"> </p>';
     
     info1.addEventListener("click", function(){
         document.getElementById('moreInfo10').innerHTML = "Half life 3 is so bad, valve hasn't even released it yet.";
@@ -97,7 +97,7 @@ item2.addEventListener('click', function(){
         document.getElementById('moreInfo4').innerHTML = "Tetris is too overcomplicated, I can't believe they couldn't make this simple.";
     } );
     info8.addEventListener("click", function(){
-        document.getElementById('moreInfo3').innerHTML = "All i have to say is that this is a ag clone.";
+        document.getElementById('moreInfo3').innerHTML = "All i have to say is that this is a tag clone.";
     } );
     info9.addEventListener("click", function(){
         document.getElementById('moreInfo2').innerHTML = "Battlefield 3 really sucks, at least with Battlefield 4, it was really good at launch, 3 was never good.";
@@ -107,88 +107,88 @@ item2.addEventListener('click', function(){
     } );
 });
 item3.addEventListener('click', function(){
-    info1.innerHTML = '<p>10. Water World</p> <img src="https://i.ytimg.com/vi/O5dRU7U37qQ/maxresdefault.jpg" width="400" title="Too red for my taste">';
-    info2.innerHTML = '<p>9. Big Rigs: Over the Road Racing</p> <img src="https://i.ytimg.com/vi/cPBamIKp8Vs/hqdefault.jpg" width="500" title="Wanna see my big rigs">';
-    info3.innerHTML = '<p>8. Air Control</p> <img src="http://i.imgur.com/7RCvaxU.jpg" width="500" title="Wasnt too satisfied with this game but I thought it was enjoyable">';
-    info4.innerHTML = '<p>7. Shaq Fu</p> <img src="http://static2.therichestimages.com/cdn/864/455/90/cw/wp-content/uploads/2014/10/Shaq-Fu2.jpg" width="500" title="Not as bad as Mortal Kombat.">';
-    info5.innerHTML = '<p>6. Meme Run</p> <img src="https://d3esbfg30x759i.cloudfront.net/ss/zlCfzSeJkHgNHvaJZD" width="500" title="Enjoy the memes">';
-    info6.innerHTML = '<p>5. Guitar Hero</p> <img src="http://webcdn.harmonixmusic.com.s3.amazonaws.com/media/uploads/Rock_Band_4_-_03.jpg" width=500px title="Not as good as Power Gig.">';
-    info7.innerHTML = '<p>4. Terraria</p> <img src="http://i.ytimg.com/vi/Mi9_3B0d7OI/maxresdefault.jpg" width="500" title="At least its not minecraft">';
-    info8.innerHTML = '<p>3. Grand Theft Auto</p> <img src="https://i.ytimg.com/vi/v5qbOTT7JvY/maxresdefault.jpg" width="500" title="Too wacky and over the top, I cant believe they put super powers in the 4th grand theft auto game. Saints Row on the other hand, incredibly realistic.">';
-    info9.innerHTML = '<p>2. Call of Duty</p> <img src="http://assets.vg247.com/current//2014/09/halo_the_master_chief_collection_3.jpg" width="500" title="Science Fiction is for nerds and sucks, but call of duty is just average.">';
-    info10.innerHTML = '<p>1. American Football</p> <img src="http://www.fondos7.net/wallpaper-original/wallpapers/pelota-de-futbol-9716.jpg" width="500" title="more of a sport than soccer.">';
+    info1.innerHTML = '<p>10. Water World</p> <img src="https://i.ytimg.com/vi/O5dRU7U37qQ/maxresdefault.jpg" width="400" title="Too red for my taste"><p id="moreInfo10"> </p>';
+    info2.innerHTML = '<p>9. Big Rigs: Over the Road Racing</p> <img src="https://i.ytimg.com/vi/cPBamIKp8Vs/hqdefault.jpg" width="500" title="Wanna see my big rigs"><p id="moreInfo9"> </p>';
+    info3.innerHTML = '<p>8. Air Control</p> <img src="http://i.imgur.com/7RCvaxU.jpg" width="500" title=""><p id="moreInfo8"> </p>';
+    info4.innerHTML = '<p>7. Shaq Fu</p> <img src="http://static2.therichestimages.com/cdn/864/455/90/cw/wp-content/uploads/2014/10/Shaq-Fu2.jpg" width="500" title=""><p id="moreInfo7"> </p>';
+    info5.innerHTML = '<p>6. Meme Run</p> <img src="https://d3esbfg30x759i.cloudfront.net/ss/zlCfzSeJkHgNHvaJZD" width="500" title="Enjoy the memes"><p id="moreInfo6"> </p>';
+    info6.innerHTML = '<p>5. Guitar Hero</p> <img src="http://webcdn.harmonixmusic.com.s3.amazonaws.com/media/uploads/Rock_Band_4_-_03.jpg" width=500px title="Not as good as Power Gig."><p id="moreInfo5"> </p>';
+    info7.innerHTML = '<p>4. Terraria</p> <img src="http://i.ytimg.com/vi/Mi9_3B0d7OI/maxresdefault.jpg" width="500" title="At least its not minecraft"><p id="moreInfo4"> </p>';
+    info8.innerHTML = '<p>3. Grand Theft Auto</p> <img src="https://i.ytimg.com/vi/v5qbOTT7JvY/maxresdefault.jpg" width="500" title=""><p id="moreInfo3"> </p>';
+    info9.innerHTML = '<p>2. Call of Duty</p> <img src="http://assets.vg247.com/current//2014/09/halo_the_master_chief_collection_3.jpg" width="500" title="Did you even see the infinite warfare trailer?"><p id="moreInfo2"> </p>';
+    info10.innerHTML = '<p>1. American Football</p> <img src="http://www.fondos7.net/wallpaper-original/wallpapers/pelota-de-futbol-9716.jpg" width="500" title="more of a sport than soccer."><p id="moreInfo1"> </p>';
     
     info1.addEventListener("click", function(){
-        document.getElementById('moreInfo10').innerHTML = "";
+        document.getElementById('moreInfo10').innerHTML = "Virtual boy was a great system, but this game was too red for my taste.";
     } );
     info2.addEventListener("click", function(){
-        document.getElementById('moreInfo9').innerHTML = "";
+        document.getElementById('moreInfo9').innerHTML = "It's ok, the great grammar really makes the game feel pretty unproffessional though.";
     } );
     info3.addEventListener("click", function(){
-        document.getElementById('moreInfo8').innerHTML = "";
+        document.getElementById('moreInfo8').innerHTML = "Wasn't too satisfied with this game but I thought it was enjoyable.";
     } );
     info4.addEventListener("click", function(){
-        document.getElementById('moreInfo7').innerHTML = "";
+        document.getElementById('moreInfo7').innerHTML = "Not as bad as Mortal Kombat but it is no where near as good as the recently released street fighter 5.";
     } );
     info5.addEventListener("click", function(){
-        document.getElementById('moreInfo6').innerHTML = "";
+        document.getElementById('moreInfo6').innerHTML = "Meme run is a fantastic game but it's on the average games list since you can't get it anymore.";
     } );
     info6.addEventListener("click", function(){
-        document.getElementById('moreInfo5').innerHTML = "";
+        document.getElementById('moreInfo5').innerHTML = "Not as bad as rock band but it isn't as good as power gig.";
     } );
     info7.addEventListener("click", function(){
-        document.getElementById('moreInfo4').innerHTML = "";
+        document.getElementById('moreInfo4').innerHTML = "It's minecraft but in 3d.";
     } );
     info8.addEventListener("click", function(){
-        document.getElementById('moreInfo3').innerHTML = "";
+        document.getElementById('moreInfo3').innerHTML = "Too wacky and over the top, I cant believe they put super powers in the 4th grand theft auto game. Saints Row on the other hand, incredibly realistic.";
     } );
     info9.addEventListener("click", function(){
-        document.getElementById('moreInfo2').innerHTML = "";
+        document.getElementById('moreInfo2').innerHTML = "Science Fiction is for nerds and sucks, but call of duty is just average.";
     } );
     info10.addEventListener("click", function(){
-        document.getElementById('moreInfo1').innerHTML = "";
+        document.getElementById('moreInfo1').innerHTML = "It's enjoyable to watch.";
     } );
 });
 item4.addEventListener('click', function(){
-    info1.innerHTML = '<p>10. AWP Asiimov</p> <img src="http://csgostash.com/img/skins/s274.png">';
-    info2.innerHTML = '<p>9. R8 Revolver Fade</p> <img src="http://csgostash.com/img/skins/s672fn.png">';
-    info3.innerHTML = '<p>8. AK-47 Vulcan</p> <img src="http://csgostash.com/img/skins/s354.png">';
-    info4.innerHTML = '<p>7. M4A4 Howl</p> <img src="http://csgostash.com/img/skins/s355.png">';
-    info5.innerHTML = '<p>6. MAC-10 Neon Rider</p> <img src="http://csgostash.com/img/skins/s553fn.png">';
-    info6.innerHTML = '<p>5. Scatter Gun</p> <img src="https://wiki.teamfortress.com/w/images/thumb/1/1b/Item_icon_Scattergun.png/100px-Item_icon_Scattergun.png?t=20120630234045" width="300">';
-    info7.innerHTML = '<p>4. Whatever this is</p> <img src="http://www.offdutygamers.com/wp-content/uploads/2012/10/MY-GUN.jpg" width="400">';
-    info8.innerHTML = '<p>3. This thing</p> <img src="http://cdn.gamerant.com/wp-content/uploads/Ghost-Recon-Future-Soldier-Gunsmith-components-570x382.jpg" width="400">';
-    info9.innerHTML = '<p>2. Sword</p> <img src="http://pngimg.com/upload/sword_PNG5521.png" width="400">';
-    info10.innerHTML = '<p>1. Beats by Dre</p> <img src="http://mydjblog.com/wp-content/uploads/Beats-by-Dr-Dre.jpg" width="400">';
+    info1.innerHTML = '<p>10. AWP Asiimov</p> <img src="http://csgostash.com/img/skins/s274.png"><p id="moreInfo10"> </p>';
+    info2.innerHTML = '<p>9. R8 Revolver Fade</p> <img src="http://csgostash.com/img/skins/s672fn.png"><p id="moreInfo9"> </p>';
+    info3.innerHTML = '<p>8. AK-47 Vulcan</p> <img src="http://csgostash.com/img/skins/s354.png"><p id="moreInfo8"> </p>';
+    info4.innerHTML = '<p>7. M4A4 Howl</p> <img src="http://csgostash.com/img/skins/s355.png"><p id="moreInfo7"> </p>';
+    info5.innerHTML = '<p>6. MAC-10 Neon Rider</p> <img src="http://csgostash.com/img/skins/s553fn.png"><p id="moreInfo6"> </p>';
+    info6.innerHTML = '<p>5. Scatter Gun</p> <img src="https://wiki.teamfortress.com/w/images/thumb/1/1b/Item_icon_Scattergun.png/100px-Item_icon_Scattergun.png?t=20120630234045" width="300"><p id="moreInfo5"> </p>';
+    info7.innerHTML = '<p>4. Whatever this is</p> <img src="http://www.offdutygamers.com/wp-content/uploads/2012/10/MY-GUN.jpg" width="400"><p id="moreInfo4"> </p>';
+    info8.innerHTML = '<p>3. This thing</p> <img src="http://cdn.gamerant.com/wp-content/uploads/Ghost-Recon-Future-Soldier-Gunsmith-components-570x382.jpg" width="400"><p id="moreInfo3"> </p>';
+    info9.innerHTML = '<p>2. Sword</p> <img src="http://pngimg.com/upload/sword_PNG5521.png" width="400"><p id="moreInfo2"> </p>';
+    info10.innerHTML = '<p>1. Beats by Dre</p> <img src="http://mydjblog.com/wp-content/uploads/Beats-by-Dr-Dre.jpg" width="400"><p id="moreInfo1"> </p>';
     
     info1.addEventListener("click", function(){
-        document.getElementById('moreInfo10').innerHTML = "";
+        document.getElementById('moreInfo10').innerHTML = "Looks bad.";
     } );
     info2.addEventListener("click", function(){
-        document.getElementById('moreInfo9').innerHTML = "";
+        document.getElementById('moreInfo9').innerHTML = "Looks worse.";
     } );
     info3.addEventListener("click", function(){
-        document.getElementById('moreInfo8').innerHTML = "";
+        document.getElementById('moreInfo8').innerHTML = "Looks even worse.";
     } );
     info4.addEventListener("click", function(){
-        document.getElementById('moreInfo7').innerHTML = "";
+        document.getElementById('moreInfo7').innerHTML = "Looks horrendous";
     } );
     info5.addEventListener("click", function(){
-        document.getElementById('moreInfo6').innerHTML = "";
+        document.getElementById('moreInfo6').innerHTML = "Looks even more horrendous.";
     } );
     info6.addEventListener("click", function(){
-        document.getElementById('moreInfo5').innerHTML = "";
+        document.getElementById('moreInfo5').innerHTML = "Looks incredibly bland.";
     } );
     info7.addEventListener("click", function(){
-        document.getElementById('moreInfo4').innerHTML = "";
+        document.getElementById('moreInfo4').innerHTML = "Even more bland.";
     } );
     info8.addEventListener("click", function(){
-        document.getElementById('moreInfo3').innerHTML = "";
+        document.getElementById('moreInfo3').innerHTML = "Completely broken.";
     } );
     info9.addEventListener("click", function(){
-        document.getElementById('moreInfo2').innerHTML = "";
+        document.getElementById('moreInfo2').innerHTML = "Melee weapons in csgo are typically really bad.";
     } );
     info10.addEventListener("click", function(){
-        document.getElementById('moreInfo1').innerHTML = "";
+        document.getElementById('moreInfo1').innerHTML = "What even is this?";
     } );
 });
